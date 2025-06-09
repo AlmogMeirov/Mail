de0393ee3b6f33098f700328dcdeec41fcb9bfe6
tomer019 <35605242+tomer019@users.noreply.github.com>
2025-06-09 23:13:39 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IP=$(hostname -I | awk '{print $1}')
-PORT="$2"
-
-echo "SERVER_IP=$IP" > /app/data/ip.env
-echo "SERVER_PORT=$PORT" >> /app/data/ip.env
-
-exec ./build/MailProject "$@"
