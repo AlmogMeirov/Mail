e6a523b40b67529b715cffacb2c4d6284a23e3cf
tomer grady <tomer0191@gmail.com>
2025-09-05 18:24:11 +0300

    tomer changes

--- src/node_server/routes/index.js
+++ src/node_server/routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+//import of the varibles
 const usersRouter = require('./authRoutes');
 const mailsRouter = require("./mailRoutes");
 const blacklistRouter = require("./blacklist"); 
 const labelsRouter = require("./labels");
-
+//made the routes
 router.use("/users", usersRouter);
 router.use("/mails", mailsRouter);
 router.use("/blacklist", blacklistRouter); 
